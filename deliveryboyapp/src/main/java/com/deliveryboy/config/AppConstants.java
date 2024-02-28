225be58140fa019648b9d146cd93639dbdbb3de1
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-02-29 00:00:20 +0530

    Kafka basic project

---
+++ deliveryboyapp/src/main/java/com/deliveryboy/config/AppConstants.java
@@ -0,0 +1,7 @@
+package com.deliveryboy.config;
+
+public class AppConstants {
+
+    public static  String LOCATION_TOPIC_NAME = "location-update-topic";
+
+}
---
+++ enduser/src/main/java/com/enduser/config/AppConstants.java
@@ -0,0 +1,13 @@
+package com.enduser.config;
+
+
+import org.apache.kafka.common.protocol.types.Field;
+
+public class AppConstants {
+
+
+        public static final String LOCATION_UPDATE_TOPIC = "location-update-topic";
+
+        public static final String GROUP_ID = "group-1";
+
+}
